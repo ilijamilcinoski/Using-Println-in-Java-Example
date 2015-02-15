0a36be36d398ad2fa12cb7552e8426eee6c08106
Ilija <milcinoskiilija@gmail.com>
2015-02-16 00:34:05 +0100

    This is an example of java code to show message such as Welcome

---
+++ src/mysecoundapplicationinjava/Welcome.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mysecoundapplicationinjava;
+
+/**
+ *The program welcome you 
+ * @author Ilija Milcinoski
+ */
+public class Welcome {
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+//Display welcome message
+System.out.println("Welcome User!");
+   } 
+}
